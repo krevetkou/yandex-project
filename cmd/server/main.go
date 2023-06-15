70f629fe1dabacf78faa7eec9413069d9457400a
Валерия Кугий <krevetkou@gmail.com>
2023-06-15 17:37:53 +0300

    iter1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -22,7 +22,7 @@ func main() {
 func handleRequest(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method not allowed", 405)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
